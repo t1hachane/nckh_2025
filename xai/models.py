178054140241816297bb69d3735b14ee40900fa4
Ha Tang <121566444+t1hachane@users.noreply.github.com>
2025-03-17 16:08:03 +0700

    he

--- xai/models.py
+++ xai/models.py
@@ -70,6 +70,7 @@ class MMDynamic(nn.Module):
                 print("pred------------", pred.shape)
                 print("p_target-------", p_target.shape)
                 print("TCPConfidence[view]-------", TCPConfidence[view].view(-1).shape, TCPConfidence[0].shape)
+                print(TCPConfidence[view].view(-1))
             confidence_loss = torch.mean(F.mse_loss(TCPConfidence[view].view(-1), p_target)+criterion(TCPLogit[view], label))
             MMLoss = MMLoss+confidence_loss
         return MMLoss, MMlogit
